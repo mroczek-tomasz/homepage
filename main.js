77c1fdcf17131b8269cd314c555c71075ea79cf1
Tomasz Mroczek <mroczek.tomasz@gmail.com>
2020-05-20 20:49:29 +0200

    Do the homework of week 4 - styling and adding some js on site

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const firstName = "Tomasz";
+const age = 37;
 
-alert(`Witam Cię na mojej stronie. Mam na imię ${firstName}`);
+
+
+console.log(`Witam Cię na mojej stronie. Mam na imię ${firstName}`);
+
+const emptyParagraph = document.querySelector('.notes__paragraph--js');
+
+emptyParagraph.innerHTML = 'Nawet udało mi się przejść do 5 tygodnia.'
+
