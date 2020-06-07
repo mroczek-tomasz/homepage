91348d2ad4c03c39f8391e92e56d8f95a9b69565
Tomasz Mroczek <mroczek.tomasz@gmail.com>
2020-06-07 14:33:41 +0200

    End of week 5

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const firstName = "Tomasz";
 const age = 37;
 
+const emptyParagraph = document.querySelector(".notes__paragraph--js");
 
+emptyParagraph.innerHTML = "Nawet udało mi się przejść do 5 tygodnia.";
 
-console.log(`Witam Cię na mojej stronie. Mam na imię ${firstName}`);
+function calculate(myNumber) {
+  console.log(`Dostałam ${myNumber}`);
+  myNumber = myNumber * 7;
+  return myNumber * 7;
+}
+const myResult = calculate(age);
 
-const emptyParagraph = document.querySelector('.notes__paragraph--js');
+console.log(myResult);
+console.log(age);
 
-emptyParagraph.innerHTML = 'Nawet udało mi się przejść do 5 tygodnia.'
-
+function greet(age, firstName) {
+  console.log(
+    `Witaj Drogi Odwiedzający, nazywam się  ${firstName} i mam ${age} lat.`
+  );
+}
+ greet(age,firstName)
